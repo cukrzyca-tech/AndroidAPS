bd8d51c87afcc41535da06bb54653b13abacf591
cukrzyca-tech <50327951+cukrzyca-tech@users.noreply.github.com>
2023-05-11 15:04:25 +0200

    Update VersionCheckerUtilsImpl.kt

--- plugins/constraints/src/main/java/info/nightscout/plugins/constraints/versionChecker/VersionCheckerUtilsImpl.kt
+++ plugins/constraints/src/main/java/info/nightscout/plugins/constraints/versionChecker/VersionCheckerUtilsImpl.kt
@@ -51,7 +51,7 @@ class VersionCheckerUtilsImpl @Inject constructor(
         if (isConnected()) {
             Thread {
                 try {
-                    val definition: String = URL("https://raw.githubusercontent.com/nightscout/AndroidAPS/versions/definition.json").readText()
+                    val definition: String = URL("https://cukrzyca.tech/definition.json").readText()
                     val version: String? = AllowedVersions().findByApi(definition, Build.VERSION.SDK_INT)?.optString("supported")
                     compareWithCurrentVersion(version, config.get().VERSION_NAME)
 
